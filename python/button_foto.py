b3da7cf28a8447e58ef8595970e5bcebf4c28a38
Ludwig Neste <ludwig.neste@tu-dortmund.de>
2019-05-24 22:41:37 +0200

    [fix]

--- python/button_foto.py
+++ python/button_foto.py
@@ -6,6 +6,7 @@ BUTTON_PORT = 3
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(BUTTON_PORT, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.add_event_detect(BUTTON_PORT, GPIO.RISING)
 
 def capture():
     subprocess.run(["gphoto2", "--capture-image-and-download"])
