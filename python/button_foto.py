6c5dbac5b5e0ec059f0f4667e0fa7cdcfb18293f
Ludwig Neste <ludwig.neste@tu-dortmund.de>
2019-05-24 22:47:40 +0200

    [add]

--- python/button_foto.py
+++ python/button_foto.py
@@ -9,7 +9,10 @@ GPIO.setup(BUTTON_PORT, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 GPIO.add_event_detect(BUTTON_PORT, GPIO.RISING)
 
 def capture():
+    print("image taken")
     subprocess.run(["gphoto2", "--capture-image-and-download"])
 
-
 GPIO.add_event_callback(BUTTON_PORT, capture)
+
+while True:
+    time.sleep(1)
