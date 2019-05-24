432b41a1a59994849369c55bd11037548d6e4e26
Ludwig Neste <ludwig.neste@tu-dortmund.de>
2019-05-24 23:05:05 +0200

    [add] hopfully first working version

--- python/button_foto.py
+++ python/button_foto.py
@@ -7,10 +7,9 @@ BUTTON_PORT = 3
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(BUTTON_PORT, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
-def capture(wat):
-    print(wat)
+def capture(channel):
     print("image taken")
-    subprocess.run(["gphoto2", "--capture-image-and-download"])
+    subprocess.call(["gphoto2", "--capture-image-and-download"])
 
 GPIO.add_event_detect(BUTTON_PORT, GPIO.FALLING, callback=capture, bouncetime=2000)
 
