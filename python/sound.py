7c1db61aad74d64f1b4fb734fa24355a1f9af95c
Noel <leonxch@gmail.com>
2019-05-25 12:00:36 +0100

    flashlight

--- python/sound.py
+++ python/sound.py
@@ -5,13 +5,19 @@ pin = 40
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(pin,GPIO.OUT)
 
+# while True:
+#     a = str(input("Do you want to change brightness? >> "))
+#     if a in ["N","n","NO","No","no"]:
+#         break
+#     elif a in ["brighter","Brighter","up","Up","+"]:
+#         GPIO.output(pin,1)
+#     elif a in ["darker","Darker","down","Down","-"]:
+#         GPIO.output(pin,0)
+#     else:
+#         print("Invalid input, please try up/down or no to exit.")
+
 while True:
-    a = str(input("Do you want to change brightness? >>"))
-    if a in ["N","n","NO","No","no"]:
-        break
-    elif a in ["brighter","Brighter","up","Up","+"]:
-        GPIO.output(pin,1)
-    elif a in ["darker","Darker","down","Down","-"]:
-        GPIO.output(pin,0)
-    else:
-        print("Invalid input, please try up/down or no to exit.")
+    time.sleep(0.1)
+    GPIO.output(pin,1)
+    time.sleep(0.1)
+    GPIO.output(pin,0)
