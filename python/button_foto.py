1cb256cf79e97853e81d23010f44f5845176e6ad
Ludwig Neste <ludwig.neste@tu-dortmund.de>
2019-05-25 12:06:12 +0200

    [add] bouncetime to 5 seconds

--- python/button_foto.py
+++ python/button_foto.py
@@ -13,18 +13,26 @@ GPIO.setup(BUTTON_PORT, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
 def save_img():
     cwd = os.getcwd()
-    filename = cwd+"/"+DIR_NAME+"/"+datetime.datetime.now().strftime("%d.%m.%Y_%H:%M:%S")+".jpg"
+    filename = cwd+"/"+DIR_NAME+"/"+datetime.datetime.now().strftime("%d.%m.%Y_%H_%M_%S")+".jpg"
 
     if not os.path.exists(cwd+"/"+DIR_NAME):
         os.makedirs(cwd+"/"+DIR_NAME)
     os.rename(cwd+"/"+IMG_NAME, filename)
 
-def capture(channel):
-    print("image taken")
-    subprocess.call(["gphoto2", "--capture-image-and-download"])
-    save_img()
+    return filename
 
-GPIO.add_event_detect(BUTTON_PORT, GPIO.FALLING, callback=capture, bouncetime=2000)
+def capture(channel):
+    subprocess.call(["gphoto2", "--capture-image-and-download"])
+    filename = save_img()
+    print("Image saved as {}".format(filename))
+    del_img()
+    return filename
+
+def del_img():
+    if os.path.isfile(os.getcwd()+"/"+IMG_NAME):
+        os.remove(os.getcwd()+"/"+IMG_NAME)
+
+GPIO.add_event_detect(BUTTON_PORT, GPIO.FALLING, callback=capture, bouncetime=5000)
 
 while True:
     time.sleep(1)
