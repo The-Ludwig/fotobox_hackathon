8610a7540f8c85a92195b51c02dfcf89e4b62118
Noel <leonxch@gmail.com>
2019-05-25 11:07:21 +0100

    interactive sound

--- python/sound.py
+++ python/sound.py
@@ -4,7 +4,12 @@ import time
 pin = 40
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(pin,GPIO.OUT)
-GPIO.output(pin,1)
 
-a=input("Do you want to quit?")
-GPIO.output(pin,0)
+while True:
+    a = input("Do you want to continue? ")
+    if a in ["N","n","NO","No","no"]:
+        break
+    elif a:
+        GPIO.output(pin,1)
+    else:
+        GPIO.output(pin,0)
