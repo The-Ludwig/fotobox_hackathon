a26da0f6ecd2b34630afb30ef35ab544ce2063f4
Noel <leonxch@gmail.com>
2019-05-26 09:24:55 +0100

    new traffic lights

---
+++ python/traffic_light.py
@@ -0,0 +1,23 @@
+import RPi.GPIO as GPIO
+import time
+
+pins = { "red":36 , "yellow":38 , "green":40 }
+
+GPIO.setmode(GPIO.BOARD)
+
+for colour in pins:
+    GPIO.setup(pins[colour],GPIO.OUT)
+
+while True:
+    time.sleep(20)
+    GPIO.output(pins[green],0)
+    GPIO.output(pins[yellow],1)
+    time.sleep(3.6)
+    GPIO.output(pins[yellow],0)
+    GPIO.output(pins[red],1)
+    time.sleep(35)
+    GPIO.output(pins[yellow],1)
+    time.sleep(1.4)
+    GPIO.output(pins[red],0)
+    GPIO.output(pins[yellow],0)
+    GPIO.output(pins[green],1)
